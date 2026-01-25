051343c1a4f29f1480bfd494f3c2a6102e3a0128
joenash156 <joenash156@gmail.com>
2026-01-25 01:52:42 +0000

    dived into struct in golang

--- 10mymaps/main.go
+++ 10mymaps/main.go
@@ -1,11 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	fmt.Println()
 
 	fmt.Println("Welcome to the lesson on Maps in Golang")
+
+	languages := make(map[string]string)
+
+	languages["JS"] = "JavaScript"
+	languages["RB"] = "Ruby"
+	languages["PY"] = "Python"
+	languages["GO"] = "Golang"
+
+	fmt.Println("List of all languages: ", languages)
+	fmt.Println("JS is short for: ", languages["JS"])
+
+	delete(languages, "RB")
+
+	fmt.Println("List of all languages: ", languages)
+
+	// loops are interesting in golang
+	for key, value := range languages {
+		fmt.Printf("For kay %v, value is %v \n", key, value)
+	}
 
 	fmt.Println()
 }
---
+++ 11mystructs/main.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println()
+
+	fmt.Println("Structs in golang")
+
+	// no inheritance in golang; no super or parent class concepts
+
+	josh := User{
+		"Joshua",
+		"joenash@gmail.com",
+		true,
+		16,
+	}
+
+	fmt.Println(josh)
+	fmt.Printf("Josh's details are: %+v \n", josh)
+
+	fmt.Printf("Name is: %v and Email is: %v \n", josh.Name, josh.Email)
+
+	var user1 User
+	var name = user1.Name
+
+	name = "Kwame"
+
+	fmt.Println(name)
+
+	fmt.Println()
+}
+
+type User struct {
+	Name   string
+	Email  string
+	Status bool
+	Age    int
+}
