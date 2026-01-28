33590538a77c1eb833d14a564cb36ff3716030df
joenash156 <joenash156@gmail.com>
2026-01-28 00:41:08 +0000

    about to look into urls

---
+++ 20urls/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+const url = "https://jek-app.onrender.com/lessons"
+
+func main() {
+	fmt.Println()
+
+	fmt.Println("Welcome to handling URLs in golang")
+	fmt.Println(url)
+
+	fmt.Println()
+}
