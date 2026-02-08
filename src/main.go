784ca43987e045e4a73bbe0bf4fc908596cd1786
joenash156 <joenash156@gmail.com>
2026-02-08 23:34:34 +0000

    learning web requests

---
+++ src/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+)
+
+func main() {
+	fmt.Println()
+
+	fmt.Println("Welcome to web server in Golang")
+	fmt.Println()
+
+	PerformGetRequest()
+
+	fmt.Println()
+}
+
+func PerformGetRequest() {
+	const myurl = "https://jek-app.onrender.com/lessons"
+
+	response, err := http.Get(myurl)
+
+	checkNilError(err)
+
+	// close response
+	defer response.Body.Close()
+
+	fmt.Println("Status code:", response.StatusCode)
+	// fmt.Println("Content length is:", response.ContentLength)
+
+	var responseString strings.Builder
+
+	content, err := io.ReadAll(response.Body)
+	checkNilError(err)
+
+	byteCount, _ := responseString.Write(content)
+	checkNilError(err)
+
+	fmt.Println("Byte count is:", byteCount)
+
+	fmt.Println(string(content))
+
+}
+
+func checkNilError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
